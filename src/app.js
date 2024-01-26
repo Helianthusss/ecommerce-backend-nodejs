a74da8ec636ffad4b9f35ddb5f4ad8c70bafde71
Helianthusss <anhtrankoc@gmail.com>
2024-01-26 17:40:36 +0700

    update project

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+
+
+// init middleware
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// init db
+require('./dbs/init.mongodb')
+const {checkOverload} = require('./helpers/check.connect')
+checkOverload()
+// init routes
+app.get('/', (req, res) => {
+    const strCompress = 'Hello Fantipjs'
+    return res.status(200).json({
+        message: 'Welcome Fantipjs!',
+        metadata: strCompress.repeat(10000)
+    })   
+})
+// handling errors
+
+module.exports = app
